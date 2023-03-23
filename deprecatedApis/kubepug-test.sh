2a04d7520c35d761db6947fb44405d2ef054fb81
Andie SAMADOULOUGOU <71939367+AnderwanSAM@users.noreply.github.com>
2023-03-22 20:40:02 -0400

    Update kubepug-test.sh

--- deprecatedApis/kubepug-test.sh
+++ deprecatedApis/kubepug-test.sh
@@ -1,3 +1,4 @@
+kubectl apply -f https://raw.githubusercontent.com/AnderwanSAM/k8s-manifests/main/deprecatedApis/flow_schema_1_29.yaml
 wget https://github.com/rikatz/kubepug/releases/download/v1.4.0/kubepug_linux_amd64.tar.gz
 tar -zxvf kubepug_linux_amd64.tar.gz
 chmod +x kubepug
